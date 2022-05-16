4b61851f58927efe94c497c171e4b1f35a83daa7
RubenLozano27 <94849547+RubenLozano27@users.noreply.github.com>
2022-05-16 13:48:53 -0400

    Finished Mid Term Practical

--- MidTermPractical/MidTermPractical.cpp
+++ MidTermPractical/MidTermPractical.cpp
@@ -1,11 +1,72 @@
 // MidTermPractical.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include<string>
 #include <iostream>
-
+bool PrintQuestion(std::string menu[], int random)
+{
+	std::string answer;
+	std::cout << menu[random] << " \n";
+	std::getline(std::cin, answer);
+	switch (random)
+	{
+	case 0:
+	{
+		if (answer == "BLUE" || answer == "Blue" || answer == "blue")
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	case 1:
+	{
+		if (answer == "blue Whale" || answer == "Blue Whale"|| answer == "blue whale")
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	case 2:
+	{
+		if (answer == "Yellow" || answer == "yellow")
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	case 3:
+	{
+		if (answer == "Brown"|| answer == "brown")
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	}
+	return false;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	std::string menu[] = { "What color is the sky?","What is the biggest animal in the world?", "Typically what color is a school bus?","What color is a snow fox during the summer?"};
+	bool ansResult = true;
+	int chosen;
+	while (ansResult)
+	{
+		chosen = rand() % 4;
+		ansResult = PrintQuestion(menu, chosen);
+		if (ansResult)
+		{
+			std::cout << "Correct! keep going!"<<std::endl;
+		}
+		else
+			std::cout << "Wrong! Game Over!"<<std::endl;
+		std::cin.ignore();
+		system("cls");
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
